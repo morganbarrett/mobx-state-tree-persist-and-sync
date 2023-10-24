0ef41a303f19e7f828ca8d4a9ba1b69cccc7eb59
morganbarrett <morganbarrett@icloud.com>
2023-10-24 16:23:33 +0100

    feat: implemented persistence store

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# `mobx-state-tree-persist-and-sync`
+# `mobx-state-tree-persistence`
+
+```javascript
+import {observer} from "mobx-react-lite";
+import {types} from "mobx-state-tree";
+import {persist} from "mobx-state-tree-persist-and-sync";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const FooModel = types.model("foo").props({
+	test: "hello world"
+});
+
+const BarModel = types.model("bar").props({
+	test: false
+});
+
+const RootModel = types.model("root").props({
+	foo: FooModel,
+	bar: BarModel
+});
+
+const rootStore = RootModel.create({
+	foo: {},
+	bar: {}
+});
+
+const persistStore = persist(rootStore, ["foo", "bar"], AsyncStorage);
+
+const App = observer(() => {
+	if (!persistStore.isRehydrated) {
+		return null;
+	}
+
+	return <Text>{rootStore.foo.test}</Text>;
+});
+
+onRemoteContent(data =>
+	persistStore.applyUpdate(rootStore, data.key, data.value)
+);
+```
--- src/index.js
+++ src/index.js
@@ -1,12 +1,58 @@
-import {persist} from "mobx-state-tree-persist";
+import {types, onSnapshot, applySnapshot} from "mobx-state-tree";
 
-export const persistAndSync = arr => {
-	for (const [store, {key}] of arr) {
-		//todo need to subscribe to changes of store and send to server
-		//todo need to subscribe to change from server and update store
-		//Patches https://mobx-state-tree.js.org/concepts/patches
-		//applySnapshot (for complete replacement of state) https://mobx-state-tree.js.org/concepts/snapshots
+const Store = types
+	.model("MobXStateTreePersistAndSyncStore")
+	.props({
+		initialized: false,
+		rehydratedMap: types.map(types.boolean)
+	})
+	.views(self => ({
+		get isRehydrated() {
+			return (
+				self.initialized &&
+				self.rehydratedMap
+					.entries()
+					.every(([_, isRehydrated]) => isRehydrated)
+			);
+		}
+	}))
+	.actions(self => ({
+		setInitialized() {
+			self.initialized = true;
+		},
+		async applyUpdate(store, key, promise) {
+			self.rehydratedMap.set(key, false);
+
+			const state = await promise;
+
+			if (state) {
+				applySnapshot(store[key], JSON.parse(state));
+			}
+
+			self.rehydratedMap.set(key, true);
+		}
+	}));
+
+const onStoreUpdate = async (stores, key, callback) => {
+	let timeout;
+
+	onSnapshot(stores[key], ({...state}) => {
+		clearTimeout(timeout);
+
+		timeout = setTimeout(() => callback(key, JSON.stringify(state)), 100);
+	});
+};
+
+export const persist = (rootStore, keys, storage) => {
+	const store = Store.create();
+
+	for (const key of keys) {
+		store.applyUpdate(rootStore, key, storage.getItem(key));
+
+		onStoreUpdate(rootStore, key, storage.setItem);
 	}
 
-	return persist(arr);
+	store.setInitialized();
+
+	return store;
 };
