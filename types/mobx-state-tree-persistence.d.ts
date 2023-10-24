f04bdbe706b81a9b371f8d1533399c981e253024
morganbarrett <morganbarrett@icloud.com>
2023-10-24 17:22:35 +0100

    fix: fixed type declaration

--- types/mobx-state-tree-persistence.d.ts
+++ types/mobx-state-tree-persistence.d.ts
@@ -1,13 +1,26 @@
-import { IModelType } from "mobx-state-tree";
-
-type Store = IModelType<{
-	isRehydrated: boolean;
-	applyUpdate: (key: string, promise: Promise<string>) => void;
-}>;
+import {IModelType, _NotCustomized} from "mobx-state-tree";
 
 type Storage = {
 	getItem(key: string): Promise<string | null>;
 	setItem(key: string, value: string): void;
 };
 
-export function persist(rootStore: Record<string, any>, keys: string[], storage: Storage): Store;
+type Store = IModelType<
+	{},
+	{
+		readonly isRehydrated: boolean;
+		applyUpdate(
+			store: IModelType<any, any>,
+			key: string,
+			promise: Promise<string>
+		): Promise<void>;
+	},
+	_NotCustomized,
+	_NotCustomized
+>;
+
+export function persist(
+	rootStore: IModelType<any, any>,
+	keys: string[],
+	storage: Storage
+): Store;
